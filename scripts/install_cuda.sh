a15d5c74605f868f6ae1967eb1ac6052cd2ed040
dzming <dzm_work@163.com>
2023-12-07 19:48:15 +0800

    修复已知bug 修复bug：添加环境变量时忘记添加export 修复bug：按照顺序，此时还未安装torch

--- scripts/install_cuda.sh
+++ scripts/install_cuda.sh
@@ -14,7 +14,5 @@ sh cuda_11.7.1_515.65.01_linux.run --silent --toolkit
 
 echo 'export PATH=$PATH:/usr/local/cuda/bin' >> ~/.bashrc
 echo 'export CUDA_HOME=$CUDA_HOME:/usr/local/cuda' >> ~/.bashrc
-echo 'LD_LIBRARY=$LD_LIBRARY:/usr/local/cuda/lib64' >> ~/.bashrc
+echo 'export LD_LIBRARY=$LD_LIBRARY:/usr/local/cuda/lib64' >> ~/.bashrc
 source ~/.bashrc
-
-python3 -c "import torch; print(torch.__version__); print(torch.cuda.is_available())"
