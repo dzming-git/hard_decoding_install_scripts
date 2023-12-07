589428206ecfca05c6900f762f3c0a7a42cfbc6e
dzming <dzm_work@163.com>
2023-12-07 19:47:32 +0800

    使用for循环优化，并添加了安装日志记录

--- scripts/install_all.sh
+++ scripts/install_all.sh
@@ -6,10 +6,26 @@
 # Description: 一键安装硬解码所需的环境
 ################################################################################
 
-bash ./install_packages.sh
-bash ./install_cuda.sh
-bash ./install_torch.sh
-bash ./install_cudnn.sh
-bash ./install_video_coded.sh
-bash ./install_ffmpeg.sh
-bash ./install_opencv.sh
+STEPS=(
+    install_packages
+    install_cuda
+    install_torch
+    install_cudnn
+    install_video_coded
+    install_ffmpeg
+    install_opencv
+)
+
+for step in "${STEPS[@]}" 
+do 
+  echo step
+  bash "./${step}.sh" >> "../logs/${step}.logs"
+done
+
+# bash ./install_packages.sh     >> ../logs/install_packages.log
+# bash ./install_cuda.sh         >> ../logs/install_cuda.log
+# bash ./install_torch.sh        >> ../logs/install_torch.log
+# bash ./install_cudnn.sh        >> ../logs/install_cudnn.log
+# bash ./install_video_coded.sh  >> ../logs/install_video_coded.log
+# bash ./install_ffmpeg.sh       >> ../logs/install_ffmpeg.log
+# bash ./install_opencv.sh       >> ../logs/install_opencv.log
