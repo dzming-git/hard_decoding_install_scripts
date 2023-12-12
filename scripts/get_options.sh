5446a5e54455dc77cc77fa9152945d2ac450f824
dzming <dzm_work@163.com>
2023-12-12 02:52:14 +0000

    修复已知bug，添加指定opencv contrib部分资源离线安装的路径 修复bug：指定GPU_COMPUTE时会报错

--- scripts/get_options.sh
+++ scripts/get_options.sh
@@ -4,21 +4,21 @@ WITH_PYTHON=false
 ORIGINAL_PACKAGE_PATH=$(realpath "../original_packages")
 DECOMPASS_PATH=$HOME
 RESERVER_DECOMPASS_FOLDER=false
-GPU_COMPUTE=0
 
 help() {
     echo "Usage:"
     echo "install_all.sh [-p] [-o ORIGINAL_PACKAGE_PATH] [-d DECOMPASS_PATH] [-r]"
     echo "Description:"
     echo "-p: Install Python related environments"
-    echo "-o ORIGINAL_PACKAGE_PATH: Specify the original package path (default: ${ORIGINAL_PACKAGE_PATH})"
-    echo "-d DECOMPASS_PATH: Specify the decompress path (default: ${DECOMPASS_PATH})"
+    echo "-o ORIGINAL_PACKAGE_PATH: Specify the original package path (default: $ORIGINAL_PACKAGE_PATH)"
+    echo "-d DECOMPASS_PATH: Specify the decompress path (default: $DECOMPASS_PATH)"
     echo "-r: Reserve the decompress folder"
     echo "-c: GPU Compute Capability, Search on https://developer.nvidia.com/cuda-gpus, if this parameter is not set, intelligent matching will be performed"
+    echo "-s: OPENCV CONTRIB SOURCES"
     exit -1
 }
 
-while getopts "po:d:rhc:" OPT; do
+while getopts "po:d:rhc:s:" OPT; do
     case $OPT in
         p) 
           WITH_PYTHON=true
@@ -38,6 +38,9 @@ while getopts "po:d:rhc:" OPT; do
         c)
           GPU_COMPUTE=$OPTARG
           ;;
+        s)
+          OPENCV_CONTRIB_SOURCES=$OPTARG
+          ;;
         \?)
           echo "Invalid option: -$OPTARG" >&2
           help
@@ -45,7 +48,7 @@ while getopts "po:d:rhc:" OPT; do
     esac
 done
 
-if (( $GPU_COMPUTE == 0 )); then
+if [ -z "$GPU_COMPUTE" ]; then
   source ./get_gpu_compute.sh
 fi
 
@@ -57,3 +60,4 @@ export ORIGINAL_PACKAGE_PATH
 export DECOMPASS_PATH
 export RESERVER_DECOMPASS_FOLDER
 export GPU_COMPUTE
+export OPENCV_CONTRIB_SOURCES
